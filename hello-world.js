30f895f1dc9845c59ba07fb39aaf6e07805de33f
Brandon Pierce <brandon@ihashacks.com>
2015-01-21 14:40:14 -0800

    single file override

--- hello-world.js
+++ hello-world.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World override\n');
 }).listen(1337, '0.0.0.0');
 console.log('Server running at http://0.0.0.0:1337/');
